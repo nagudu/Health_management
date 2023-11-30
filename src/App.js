6836b998cf68ac5fe5f50a8aae9adfcdf4af4ada
nagudu <halifashuaibu12@gmail.com.com>
2023-11-30 16:35:42 +0100

    save

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-import AppNavigation from './Routes/AppNavigation';
+import logo from "./logo.svg";
+import "./App.css";
+import AppNavigation from "./Routes/AppNavigation";
 
 function App() {
   return (
     <div>
-    <AppNavigation />
+      <AppNavigation />
     </div>
   );
 }
