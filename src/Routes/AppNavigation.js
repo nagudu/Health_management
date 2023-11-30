15d78e94755adb3a5bd1caad7298f07f87d9015c
uniboika <nazifabdullahi003@gmail.com>
2023-11-30 17:30:17 +0100

    save

--- src/Routes/AppNavigation.js
+++ src/Routes/AppNavigation.js
@@ -1,11 +1,12 @@
 import { useRoutes } from "react-router-dom";
 import SingIn from "../Componet/SingIn";
+import Home from "../Componet/Home";
 
 function AppNavigation() {
     let element = useRoutes([
     {
         path:'/',
-        element: <SingIn />
+        element:<h1>88888</h1>
     }
     ])
 
